13b82333ed3589f3e6d320502bf102def1222bb7
beatrizsmerino <beatrizsmerino@gmail.com>
2019-06-07 23:05:38 +0200

    Fixed bug. The method remove user didn't work

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -23,8 +23,8 @@ export default {
 	props: ["users"],
 	methods: {
 		userRemove(userToRemove) {
-			this.users.splice(userToRemove, 1);
-		}
+			this.users.splice(this.users.indexOf(userToRemove), 1);
+		},
 	}
 };
 </script>
