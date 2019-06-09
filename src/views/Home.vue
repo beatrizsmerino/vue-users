c43aca074f2ffdb5846942ef205943560b2c84be
beatrizsmerino <beatrizsmerino@gmail.com>
2019-06-09 15:37:22 +0200

    Added info and styles css to view home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
 	<div class="home">
-
+		<div class="logo-vue">
+			<img src="../assets/logo.png" alt="Vue logo">
+		</div>
+		<div class="definition">
+			<p>
+				List of users with vue.js
+			</p>
+		</div>
 	</div>
 </template>
 
 
 
 <script>
-
 export default {
 	components: {	
 	},
-	props: [],
+	props: ["users"],
 	methods: {
 	}
 };
 </script>
+
+
+
+<style lang="scss">
+.logo-vue {
+  text-align: center;
+}
+.definition{
+	text-align: center;
+	color: var(--color-brand-1);
+	font-size: 1.5rem;
+	font-weight: 300;
+}
+</style>
