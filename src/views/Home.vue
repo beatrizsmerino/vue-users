ee7e3ed96bb23cf008622cde894f7c99d42ac8ac
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-03-31 04:40:14 +0200

    Updated subtitle and description of Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,22 @@
 <template>
 	<div class="page-main__inner">
 		<section class="home">
+			<h2 class="subtitle">
+				List of users with vue.js
+			</h2>
+			<div class="description">
+				<p>
+					Content developed with the API<br>
+					<a href="http://randomuser.me" target="_blank" rel="noopener noreferrer">
+						https://randomuser.me
+					</a>
+				</p>
+			</div>
 			<div class="logo-vue">
 				<img
 					src="../assets/logo.png"
 					alt="Vue logo"
 				/>
-			</div>
-			<div class="definition">
-				<p>
-					List of users with vue.js
-				</p>
 			</div>
 		</section>
 	</div>
@@ -31,13 +37,29 @@
 
 
 <style lang="scss" scoped>
+	.subtitle {
+		text-align: center;
+		font-family: "Dauphin", "Oxygen", Helvetica, sans-serif;
+		font-size: 3rem;
+		color: var(--color-brand-1);
+	}
+
+	.description {
+		text-align: center;
+		font-size: 1.5rem;
+		color: var(--color-brand-2);
+
+		a {
+			font-weight: bold;
+			color: var(--color-brand-2);
+
+			&:hover {
+				color: var(--color-brand-1);
+			}
+		}
+	}
+
 	.logo-vue {
 		text-align: center;
-	}
-	.definition {
-		text-align: center;
-		color: var(--color-brand-1);
-		font-size: 1.5rem;
-		font-weight: 300;
 	}
 </style>
