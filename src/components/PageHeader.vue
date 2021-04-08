50ae42771b4666166a5397827c928b4d56ef25a2
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-04-08 02:26:06 +0200

    Added open and close nav when click nav button and resized the window

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -6,21 +6,37 @@
 		>
 			VueUsers
 		</router-link>
-		<nav class="nav">
-			<router-link
-				to="/"
-				class="nav__item"
-				exact
+		<nav
+			class="nav"
+			:class="{'is-open': isOpen, 'is-animated': isAnimated}"
+		>
+			<ul class="nav__list">
+				<li class="nav__item">
+					<router-link
+						to="/"
+						class="nav__item"
+						exact
+					>
+						Home
+					</router-link>
+				</li>
+				<li class="nav__item">
+
+					<router-link
+						to="/users"
+						class="nav__item"
+						:class="{'router-link-active': $route.path.includes('user')}"
+					>
+						Users
+					</router-link>
+				</li>
+			</ul>
+			<Button
+				class="nav__button button button--icon"
+				@button-click="openCloseNav"
 			>
-				Home
-			</router-link>
-			<router-link
-				to="/users"
-				class="nav__item"
-				:class="{'router-link-active': $route.path.includes('user')}"
-			>
-				Users
-			</router-link>
+				<i class="fa fa-bars"></i>
+			</Button>
 		</nav>
 	</header>
 </template>
@@ -28,8 +44,51 @@
 
 
 <script>
+	import Button from "./Button";
+
 	export default {
 		name: 'PageHeader',
+		components: {
+			Button
+		},
+		data() {
+			return {
+				isAnimated: false,
+				isOpen: false
+			}
+		},
+		methods: {
+			openCloseNav() {
+				this.isAnimated = true;
+				this.isOpen = !this.isOpen;
+			},
+			closeNav() {
+				this.isOpen = false;
+			},
+			handleResize(event) {
+				const maxWidthBreakpointMD = 768;
+				if (event.target.outerWidth >= maxWidthBreakpointMD) {
+					this.isAnimated = false;
+					this.closeNav();
+				}
+			}
+		},
+		watch: {
+			$route(to, from) {
+				if (to !== from) {
+					this.closeNav();
+				}
+			},
+			isOpen: function () {
+				document.body.style.overflow = this.isOpen ? 'hidden' : ''
+			}
+		},
+		created() {
+			window.addEventListener('resize', this.handleResize);
+		},
+		destroyed() {
+			window.removeEventListener('resize', this.handleResize);
+		}
 	};
 </script>
 
@@ -46,11 +105,11 @@
 		position: fixed;
 		top: 0;
 		left: 0;
-		z-index: 999;
+		z-index: 999999;
 		background-color: $color-brand-1;
 		box-shadow: 0 0 1rem 0.5rem rgba($color-black, 0.3);
 
-		@include media("md") {
+		@include media("sm") {
 			height: 6rem;
 			padding: 0 2rem;
 		}
@@ -68,7 +127,11 @@
 		line-height: 100%;
 		color: $color-brand-2;
 
-		@include media("md") {
+		&:hover {
+			color: $color-white;
+		}
+
+		@include media("sm") {
 			font-size: 4rem;
 		}
 	}
@@ -76,8 +139,28 @@
 	.nav {
 		display: flex;
 		justify-content: center;
-		font-size: 2.4rem;
-		font-weight: 600;
+
+		&__list {
+			width: 100%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size: 2.4rem;
+			font-weight: 600;
+
+			@include media("sm") {
+				height: calc(100% - 6rem);
+				padding: 10rem 2rem;
+				flex-direction: column;
+				justify-content: flex-start;
+				position: fixed;
+				top: 6rem;
+				left: 0;
+				z-index: 99;
+				transform: translate3d(100%, 0, 0);
+				background: $color-gradient;
+			}
+		}
 
 		&__item {
 			padding: 0 0.2rem;
@@ -88,6 +171,11 @@
 
 			&:not(:last-child) {
 				margin-right: 2rem;
+
+				@include media("sm") {
+					margin-right: 0;
+					margin-bottom: 2rem;
+				}
 			}
 
 			&.router-link-active,
@@ -109,8 +197,36 @@
 			}
 		}
 
-		@include media("md") {
+		&__button {
 			display: none;
+			font-size: 2.5rem;
+			color: $color-brand-2;
+
+			&:hover {
+				color: $color-white;
+			}
+
+			@include media("sm") {
+				display: flex;
+			}
+		}
+
+		&.is-animated {
+			.nav{
+				&__list {
+					transition: all 0.5s ease-in-out 0s;
+				}
+			}
+		}
+
+		&.is-open {
+			.nav {
+				background-color: transparent;
+
+				&__list {
+					transform: translate3d(0, 0, 0);
+				}
+			}
 		}
 	}
 </style>
