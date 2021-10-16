1c20106d3c596d5a735eaeaa6e89c022f4deef42
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-10-16 21:34:20 +0200

    REFACTORING. Created 'Logo' component and use it on the 'Header' component

---
+++ src/components/Logo.vue
@@ -0,0 +1,33 @@
+<template>
+	<router-link
+		to="/"
+		class="logo"
+	>
+		VueUsers
+	</router-link>
+</template>
+
+<script>
+	export default {
+		name: 'Logo'
+	}
+</script>
+
+<style lang="scss" scoped>
+	.logo {
+		display: inline-block;
+		font-family: "Dauphin", "Oxygen", Helvetica, sans-serif;
+		font-size: 5rem;
+		font-weight: bold;
+		line-height: 100%;
+		color: $color-brand-2;
+
+		&:hover {
+			color: $color-white;
+		}
+
+		@include media("sm") {
+			font-size: 4rem;
+		}
+	}
+</style>
