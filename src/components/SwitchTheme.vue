4e7e03a2543894d985dc6e630557719586d4628c
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-10-16 22:22:29 +0200

    SWITCH THEME. Created 'switch-theme' component to change the theme from light to dark and the other way round.

---
+++ src/components/SwitchTheme.vue
@@ -0,0 +1,108 @@
+<template>
+	<label
+		for="switchTheme"
+		class="switch-theme"
+		@change="switchTheme"
+	>
+		<input
+			id="switchTheme"
+			class="switch-theme__field"
+			type="checkbox"
+		>
+		<span class="switch-theme__bullet switch-theme__bullet--rounded"></span>
+	</label>
+</template>
+
+<script>
+	export default {
+		name: 'SwitchTheme',
+		methods: {
+			switchTheme: function (event) {
+				const field = event.target;
+				const page = document.querySelector("html");
+				if (page && field.checked) {
+					page.classList.remove("is-theme-light");
+					page.classList.add("is-theme-dark");
+				} else {
+					page.classList.remove("is-theme-dark");
+					page.classList.add("is-theme-light");
+				}
+			},
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.switch-theme {
+		width: 6rem;
+		height: 3rem;
+		display: inline-block;
+		position: relative;
+
+		&__field {
+			width: 2rem;
+			height: 2rem;
+			position: absolute;
+			top: 50%;
+			left: 0.5rem;
+			transform: translate(0, -50%);
+			opacity: 0;
+
+			&:checked + .switch-theme__bullet:before {
+				left: initial;
+				right: 0.5rem;
+				background-color: $color-brand-2;
+			}
+		}
+
+		&__bullet {
+			width: 100%;
+			height: 100%;
+			display: inline-block;
+			position: absolute;
+			top: 0;
+			left: 0;
+			background-color: $color-brand-1;
+			box-shadow: inset 0 0 1rem rgba($color-black, 0.5);
+			transition: 0.4s;
+			cursor: pointer;
+
+			&:before {
+				content: "";
+				width: 2rem;
+				height: 2rem;
+				display: inline-block;
+				position: absolute;
+				left: 0.5rem;
+				top: 50%;
+				transform: translate(0, -50%);
+				background-color: $color-white;
+				transition: 0.4s;
+			}
+
+			&--rounded {
+				border-radius: 2rem;
+
+				&:before {
+					border-radius: 50%;
+				}
+			}
+		}
+
+		&:hover {
+			.switch-theme {
+				&__field {
+					&:checked + .switch-theme__bullet:before {
+						background-color: $color-brand-2;
+					}
+				}
+
+				&__bullet {
+					&:before {
+						background-color: $color-white;
+					}
+				}
+			}
+		}
+	}
+</style>
