39cc7fdecea6967ecee701a3685cf49e9a93b165
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-10-25 16:06:02 +0200

    REFACTORING. Created vue 'plugins' for build global methods of the functions Local Storage

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import { tools } from './plugins/global-methods.js';
 import './assets/images/icons/icons-import.js';
+
+Vue.prototype.$tools = tools;
 
 import Icon from './components/UI/Icon';
 Vue.component('Icon', Icon);
