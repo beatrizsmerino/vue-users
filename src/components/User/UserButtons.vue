941423f3bce79326af14df70ac33afe8942b0751
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-11-08 00:17:13 +0100

    USER. Added animation to the buttons in their hover state
    - Moved buttons to the list, added 'user-buttons__list' and 'user-buttons__item'
    - Added svg to create a curve over the buttons in their hover state

--- src/components/User/UserButtons.vue
+++ src/components/User/UserButtons.vue
@@ -1,71 +1,99 @@
 <template>
 	<div class="user-buttons">
-		<Button
-			class="user-buttons__item button--bg-brand-2"
-			:class="{ 'button--circle': isMobile }"
-			:title="showUserInfo ? 'SHOW USER INFO' : 'HIDE USER INFO'"
-			@button-click="showHideInfoUserEmit"
+		<ul class="user-buttons__list">
+			<li class="user-buttons__item">
+				<Button
+					class="button--bg-brand-2"
+					:class="{ 'button--circle': isMobile }"
+					:title="showUserInfo ? 'SHOW USER INFO' : 'HIDE USER INFO'"
+					@button-click="showHideInfoUserEmit"
+				>
+					<span class="button__icon">
+						<i
+							class="fa"
+							:class="showUserInfo ? 'fa-eye' : 'fa-eye-slash'"
+						/>
+					</span>
+					<span
+						v-if="!isMobile"
+						class="button__text"
+						v-html="
+							showUserInfo ? 'SHOW USER INFO' : 'HIDE USER INFO'
+						"
+					/>
+				</Button>
+			</li>
+			<li class="user-buttons__item">
+				<Button
+					class="button--bg-brand-2"
+					:class="{ 'button--circle': isMobile }"
+					title="ORDER USERS"
+					@button-click="orderUsersEmit"
+				>
+					<span class="button__icon">
+						<i class="fa fa-sort" />
+					</span>
+					<span
+						v-if="!isMobile"
+						class="button__text"
+					>
+						ORDER USERS
+					</span>
+				</Button>
+			</li>
+			<li class="user-buttons__item">
+				<Button
+					class="button--bg-brand-2"
+					:class="{ 'button--circle': isMobile }"
+					title="GET USERS"
+					@button-click="getUsersEmit"
+				>
+					<span class="button__icon">
+						<i class="fa fa-user-plus" />
+					</span>
+					<span
+						v-if="!isMobile"
+						class="button__text"
+					>
+						GET USERS
+					</span>
+				</Button>
+			</li>
+			<li class="user-buttons__item">
+				<Button
+					class="button--bg-brand-2"
+					:class="{ 'button--circle': isMobile }"
+					title="REMOVE ALL USERS"
+					@button-click="removeAllUsersEmit"
+				>
+					<span class="button__icon">
+						<i class="fa fa-user-minus" />
+					</span>
+					<span
+						v-if="!isMobile"
+						class="button__text"
+					>
+						REMOVE ALL USERS
+					</span>
+				</Button>
+			</li>
+		</ul>
+
+		<svg
+			class="user-buttons__svg"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 202.9 45.5"
 		>
-			<span class="button__icon">
-				<i
-					class="fa"
-					:class="showUserInfo ? 'fa-eye' : 'fa-eye-slash'"
+			<clipPath
+				id="menuCurve"
+				clipPathUnits="objectBoundingBox"
+				transform="scale(0.0049285362247413 0.021978021978022)"
+			>
+				<path
+					d="M6.7,45.5c5.7,0.1,14.1-0.4,23.3-4c5.7-2.3,9.9-5,18.1-10.5c10.7-7.1,11.8-9.2,20.6-14.3c5-2.9,9.2-5.2,15.2-7 c7.1-2.1,13.3-2.3,17.6-2.1c4.2-0.2,10.5,0.1,17.6,2.1c6.1,1.8,10.2,4.1,15.2,7c8.8,5,9.9,7.1,20.6,14.3c8.3,5.5,12.4,8.2,18.1,10.5 c9.2,3.6,17.6,4.2,23.3,4H6.7z"
 				/>
-			</span>
-			<span
-				v-if="!isMobile"
-				class="button__text"
-				v-html="showUserInfo ? 'SHOW USER INFO' : 'HIDE USER INFO'"
-			/>
-		</Button>
-		<Button
-			class="user-buttons__item button--bg-brand-2"
-			:class="{ 'button--circle': isMobile }"
-			title="ORDER USERS"
-			@button-click="orderUsersEmit"
-		>
-			<span class="button__icon">
-				<i class="fa fa-sort" />
-			</span>
-			<span
-				v-if="!isMobile"
-				class="button__text"
-			>
-				ORDER USERS
-			</span>
-		</Button>
-		<Button
-			class="user-buttons__item button--bg-brand-2"
-			:class="{'button--circle': isMobile }"
-			title="GET USERS"
-			@button-click="getUsersEmit"
-		>
-			<span class="button__icon">
-				<i class="fa fa-user-plus" />
-			</span>
-			<span
-				v-if="!isMobile"
-				class="button__text"
-			>
-				GET USERS
-			</span>
-		</Button>
-		<Button
-			class="user-buttons__item button--bg-brand-2"
-			:class="{ 'button--circle': isMobile }"
-			title="REMOVE ALL USERS"
-			@button-click="removeAllUsersEmit"
-		>
-			<span class="button__icon">
-				<i class="fa fa-user-minus" />
-			</span>
-			<span
-				v-if="!isMobile"
-				class="button__text"
-			>
-				REMOVE ALL USERS
-			</span>
-		</Button>
+			</clipPath>
+		</svg>
 	</div>
 </template>
 
@@ -134,27 +162,80 @@
 
 <style lang="scss" scoped>
 	.user-buttons {
-		display: flex;
 		position: fixed;
 		z-index: 999;
 		bottom: 0;
 		left: 0;
-		flex-wrap: wrap;
-		align-items: center;
-		justify-content: center;
 		width: 100%;
 		padding: 1rem;
 		background-color: $color-brand-1;
 		box-shadow: 0 0 1rem 0.5rem rgba($color-black, 0.3);
 
+		&__list {
+			display: flex;
+			flex-wrap: wrap;
+			align-items: center;
+			justify-content: center;
+			list-style: none;
+		}
+
 		&__item {
+			position: relative;
+			transition: 0.5s ease-in-out 0s;
+
 			&:not(:last-child) {
 				margin-right: 1rem;
+
+				@include media('md') {
+					margin-right: 3rem;
+				}
 			}
+
+			&:after {
+				content: '';
+				display: inline-block;
+				position: absolute;
+				z-index: -1;
+				top: 0;
+				left: 50%;
+				width: 14rem;
+				height: 2.5rem;
+				transform: translate3d(-50%, 100%, 0);
+				transition: all 0.5s ease-in-out 0s;
+				opacity: 0;
+				background-color: $color-brand-1;
+				clip-path: url('#menuCurve');
+				will-change: transform;
+			}
+
+			&:hover {
+				&:after {
+					@include media('md') {
+						transform: translate3d(-50%, -3.5rem, 0);
+						opacity: 1;
+					}
+				}
+
+				::v-deep {
+					.button {
+						@include media('md') {
+							transform: translate3d(0, -1rem, 0) scale(1.4);
+						}
+					}
+				}
+			}
+		}
+
+		&__svg {
+			position: absolute;
+			width: 0;
+			height: 0;
 		}
 
 		::v-deep {
 			.button {
+				transform: scale(1);
+
 				&__icon {
 					font-size: 2rem;
 				}
