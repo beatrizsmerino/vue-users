befe938a0c7d56ddc087d0241d8c2168cbbe5c4a
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2025-03-12 22:02:17 +0100

    fix(config): found commitlint error about the `header-max-length` rule when pushing changes
    Error:
    - ✖ header must not be longer than 100 characters, current length is 110 [header-max-length]
    ✖ Found 1 problems, 0 warnings.
    
    Solution:
    - In the `commitlint.config.js` configuration file, increase the maximum number of characters in the `header-max-length` rule, to write a longer commit title.

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -3,6 +3,11 @@ export default {
 		"@commitlint/config-conventional",
 	],
 	"rules": {
+		"header-max-length": [
+			2,
+			"always",
+			150,
+		],
 		"body-max-line-length": [
 			2,
 			"always",
