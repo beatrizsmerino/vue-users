30af53736f980bb8d24b6b94c12662d6db964d5d
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2025-06-10 22:04:26 +0200

    refactor(UserDetail): create `UserData` component to display user fields with icon, key and value
    Reason:
    - Refactors `UserDetail` to use the new `UserData` component for cleaner markup and improved reusability when rendering individual pieces of user information.

---
+++ src/components/User/UserData.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="user-data">
+		<h4 class="user-data__key">
+			<span class="user-data__icon">
+				<i :class="userDataIcon"></i>
+			</span>
+			<strong>{{ userDataKey }}:</strong>
+		</h4>
+		<p class="user-data__value">
+			{{ userDataValue }}
+		</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		"name": "UserData",
+		"props": {
+			"userDataIcon": {
+				"type": String,
+				"required": true,
+			},
+			"userDataKey": {
+				"type": String,
+				"required": true,
+			},
+			"userDataValue": {
+				"type": String,
+				"required": true,
+			},
+		},
+		data() {
+			return {};
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.user-data {
+		display: flex;
+		flex-direction: column;
+
+		&__icon {
+			margin-right: 0.5rem;
+			color: $color-brand-1;
+		}
+
+		&__key {
+			margin-bottom: 0.5rem;
+			color: $color-brand-1;
+			font-size: 1.8rem;
+		}
+
+		&__value {
+			color: $color-light;
+			font-size: 1.6rem;
+			word-break: break-all;
+		}
+	}
+</style>
