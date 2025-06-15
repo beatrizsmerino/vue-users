0c39f01b29519de8e957ffc6993196ba82b474b6
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2025-06-15 17:26:55 +0200

    refactor(Map): extract popup content from `MapLeafletMarker` into reusable `MapMarkerPosition` component
    Steps:
    - Create new `MapMarkerPosition` component to display popup content with coordinates.
    - Replace popup content in `MapLeafletMarker` with `MapMarkerPosition`.
    - Move styles and logic from `MapLeafletMarker` component to `MapMarkerPosition`.
    
    Reason:
    - Consolidate popup content into a single reusable component.
    - Simplify maintenance and ensures consistent information display across map types.
    - `MapMarkerPosition` will also be registered and used in the `MapGoogleMarker` component.

---
+++ src/components/Map/MapMarkerPosition.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="map-popup">
+		<h6 class="map-popup__title">
+			Current location:
+		</h6>
+		<div class="map-popup__content">
+			<p>
+				<strong>Latitude:</strong>
+				{{ position.lat }}
+			</p>
+			<p>
+				<strong>Longitude:</strong>
+				{{ position.lng }}
+			</p>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		"name": "MapMarkerPosition",
+		"props": {
+			"position": {
+				"type": Object,
+				"required": true,
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.map-popup {
+		display: flex;
+		flex-direction: column;
+		width: fit-content;
+		font-size: 1.5rem;
+		gap: 1.5rem;
+
+		&__title {
+			font-weight: bold;
+		}
+
+		&__content {
+			display: flex;
+			flex-direction: column;
+			gap: 0.5rem;
+
+			p {
+				margin: 0;
+			}
+
+			strong {
+				color: $color-brand-1;
+			}
+		}
+	}
+</style>
