fcfddc915a0d4be60b70816a55695a1d082060bb
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2025-06-15 18:41:12 +0200

    fix(MapGoogle): add missing coordinate popup using `MapMarkerPosition` in `MapGoogleMarker`
    Steps:
    - Add popup to `MapGoogleMarker` to display latitude and longitude on marker click.
    - Reuse `MapMarkerPosition` component for consistency with Leaflet markers.
    
    Reason:
    - Align Google Maps marker behavior with Leaflet by showing coordinates on click.
    - Prevent code duplication by reusing existing shared component.

--- src/components/Map/MapGoogle/MapGoogleMarker.vue
+++ src/components/Map/MapGoogle/MapGoogleMarker.vue
@@ -1,12 +1,19 @@
 <template>
-	<div></div>
+	<MapMarkerPosition
+		ref="googleMapsPopupRef"
+		:position="marker.position"
+	/>
 </template>
 
 <script>
 	import { MAP_GOOGLE_MARKER_ICON } from "@/constants/MapSettings";
+	import MapMarkerPosition from "@/components/Map/MapMarkerPosition";
 
 	export default {
 		"name": "MapGoogleMarker",
+		"components": {
+			MapMarkerPosition,
+		},
 		"props": {
 			"google": {
 				"type": Object,
@@ -22,13 +29,29 @@
 			},
 		},
 		mounted() {
-			const { Marker } = this.google.maps;
+			const { Marker, InfoWindow } = this.google.maps;
 
-			new Marker({
-				"position": this.marker.position,
-				"marker": this.marker,
-				"map": this.map,
-				"icon": MAP_GOOGLE_MARKER_ICON,
+			this.$nextTick(() => {
+				const infoWindowContent = this.$refs.googleMapsPopupRef?.$el;
+
+				if (infoWindowContent?.parentNode) {
+					infoWindowContent.parentNode.removeChild(infoWindowContent);
+				}
+
+				const infowindow = new InfoWindow({
+					"content": infoWindowContent,
+				});
+
+				const marker = new Marker({
+					"position": this.marker.position,
+					"marker": this.marker,
+					"map": this.map,
+					"icon": MAP_GOOGLE_MARKER_ICON,
+				});
+
+				marker.addListener("click", () => {
+					infowindow.open(this.map, marker);
+				});
 			});
 		},
 	};
